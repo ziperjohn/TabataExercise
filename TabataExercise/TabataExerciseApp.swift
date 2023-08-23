bcbf2ccb5cbe628cddeac27da53b28cac0bbf451
ziperjohn <honzavaverka1996@gmail.com>
2023-08-23 16:34:21 +0200

    Initial Commit

---
+++ TabataExercise/TabataExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabataExerciseApp.swift
+//  TabataExercise
+//
+//  Created by Jan Vaverka on 23.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct TabataExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
