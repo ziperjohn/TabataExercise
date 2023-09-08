e134f6939d1eb87012736032cec28a9022b0e663
ziperjohn <honzavaverka1996@gmail.com>
2023-09-08 08:46:44 +0200

    Fixed typo

--- TabataExercise/Environment/TabataObservableObject.swift
+++ TabataExercise/Environment/TabataObservableObject.swift
@@ -11,7 +11,7 @@ class TabataObservableObject: ObservableObject {
     @Published var tabataModel: TabataModel
 
     init() {
-        tabataModel = TabataModel(countdown: 3, warmup: 30, exercise: 20, rest: 10, recovery: 15, cooldown: 30, sets: 3, cycles: 2, isSoundEnabled: true, countdownSound: .beep, warmupSound: .airhorn, exerciseSound: .gooo, restSound: .relax, recoverySound: .ta, cooldownSound: .boxingBell, finishSound: .trippleAirhorn)
+        tabataModel = TabataModel(countdown: 3, warmup: 30, exercise: 20, rest: 10, recovery: 15, cooldown: 30, sets: 3, cycles: 2, isSoundEnabled: true, countdownSound: .beep, warmupSound: .airhorn, exerciseSound: .gooo, restSound: .relax, recoverySound: .tada, cooldownSound: .boxingBell, finishSound: .trippleAirhorn)
     }
 
     var workoutTime: Int {
